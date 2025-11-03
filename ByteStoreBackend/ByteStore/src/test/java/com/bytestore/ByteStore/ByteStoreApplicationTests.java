ece0eecfea595fb635be002dc768a3b5843a63e8
troyeta05 <leotroyeta05@gmail.com.com>
2025-11-03 06:59:15 -0600

    Primer commit: estructura inicial del proyecto ByteStore (Solo Backend)

---
+++ ByteStoreBackend/ByteStore/src/test/java/com/bytestore/ByteStore/ByteStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytestore.ByteStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
